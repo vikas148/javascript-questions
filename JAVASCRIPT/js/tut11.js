9c26a4cc1a2a4d1a520327776c7e61ce0a575b7d
VIKAS BAIRWA <71213518+Cyber420-india@users.noreply.github.com>
2022-06-10 22:46:13 +0530

    learnt some windows object methoda

---
+++ JAVASCRIPT/js/tut11.js
@@ -0,0 +1,13 @@
+console.log('we are inside tut11');
+ 
+// let a=window;
+let a=window.document;
+// window.alert('hello harry')//you can use alert also without window
+// a=prompt('you are finished');
+// a=confirm('are you sure to fuck your future');
+// a=window.innerHeight;
+// a=window.innerWidth;//use it without window
+// a=location;
+a=location.toString();
+a=window.history;
+console.log(a);
